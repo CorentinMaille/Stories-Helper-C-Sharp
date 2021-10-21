b389b673341508ff6367bd02d5f0655a998560cb
CorentinMaille <maillecorentin@pm.me>
2021-10-21 14:46:43 +0200

    first commit

---
+++ StoriesHelper/pageConnexion.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace StoriesHelper
+{
+    partial class pageConnexion
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titre = new System.Windows.Forms.Label();
+            this.emailInput = new System.Windows.Forms.TextBox();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.connexion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titre
+            // 
+            this.titre.AutoSize = true;
+            this.titre.Font = new System.Drawing.Font("Modern No. 20", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titre.Location = new System.Drawing.Point(145, 104);
+            this.titre.Name = "titre";
+            this.titre.Size = new System.Drawing.Size(724, 45);
+            this.titre.TabIndex = 0;
+            this.titre.Text = "Bienvenue sur StoriesHelper BackOffice";
+            // 
+            // emailInput
+            // 
+            this.emailInput.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailInput.Location = new System.Drawing.Point(434, 227);
+            this.emailInput.Name = "emailInput";
+            this.emailInput.Size = new System.Drawing.Size(252, 27);
+            this.emailInput.TabIndex = 1;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailLabel.Location = new System.Drawing.Point(270, 229);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(124, 23);
+            this.emailLabel.TabIndex = 2;
+            this.emailLabel.Text = "Adresse Email :";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(300, 286);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(94, 23);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password : ";
+            // 
+            // passwordInput
+            // 
+            this.passwordInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordInput.Location = new System.Drawing.Point(434, 286);
+            this.passwordInput.Name = "passwordInput";
+            this.passwordInput.PasswordChar = '*';
+            this.passwordInput.Size = new System.Drawing.Size(252, 27);
+            this.passwordInput.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(282, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(435, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Veuillez vous connecter pour pouvoir accéder à votre BackOffice";
+            // 
+            // connexion
+            // 
+            this.connexion.Font = new System.Drawing.Font("Modern No. 20", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connexion.Location = new System.Drawing.Point(491, 346);
+            this.connexion.Name = "connexion";
+            this.connexion.Size = new System.Drawing.Size(131, 40);
+            this.connexion.TabIndex = 6;
+            this.connexion.Text = "connexion";
+            this.connexion.UseVisualStyleBackColor = true;
+            this.connexion.Click += new System.EventHandler(this.connexion_Click);
+            // 
+            // pageConnexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 550);
+            this.Controls.Add(this.connexion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.passwordInput);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.emailInput);
+            this.Controls.Add(this.titre);
+            this.Name = "pageConnexion";
+            this.Text = "pageConnexion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titre;
+        private System.Windows.Forms.TextBox emailInput;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox passwordInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button connexion;
+    }
+}
+
---
+++ StoriesHelper/pageConnexion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using BC = BCrypt.Net.BCrypt;
+
+namespace StoriesHelper
+{
+    public partial class pageConnexion : Form
+    {
+        MySqlConnection conn = new MySqlConnection("database=projet;server=localhost;user id = root;pwd=");
+        public pageConnexion()
+        {
+            InitializeComponent();
+            MySqlCommand command = conn.CreateCommand();
+
+            try
+            {
+                conn.Open();
+                MessageBox.Show("Connexion effectuée !");
+            } catch {
+                MessageBox.Show("Connexion échouée !");
+            }
+        }
+
+        private void connexion_Click(object sender, EventArgs e)
+        {
+            MySqlCommand command = conn.CreateCommand();
+            string mdp = passwordInput.Text;
+            string email = emailInput.Text;
+            command.Parameters.AddWithValue("@email", email);
+            command.CommandText = "SELECT password FROM organizations WHERE email = @email";
+            MySqlDataReader reader = command.ExecuteReader();
+            string mdpHash = "";
+            MessageBox.Show(mdp);
+            while (reader.Read())
+            {
+                mdpHash = reader.GetString(0);
+            }
+            MessageBox.Show(mdpHash);
+            bool verified = BC.Verify(mdp, mdpHash);
+            if (verified)
+            {
+                MessageBox.Show("Le mot de passe est correct");
+            } else
+            {
+                MessageBox.Show("Le mot de passe est incorrect");
+            }
+        }
+    }
+}
